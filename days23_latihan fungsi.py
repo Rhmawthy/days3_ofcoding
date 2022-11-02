f69fa002c14bd09b9a4bf5c38ed46e1f4a2dad59
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-11-02 08:03:08 -0700

    Add files via upload

---
+++ days23_latihan fungsi.py
@@ -0,0 +1,10 @@
+#menghitung menggunakan fungsi
+angka = int (input ("masukkan angka:"))
+
+def hitung (number = 0):
+    while number < angka:
+        number += 1
+        print (number)
+
+    
+hitung ( number = 0)
